be2ad4163d3ebb2bafbb15f89199b5a8b46716bf
SeongIkKim <kpic1638@gmail.com>
2020-04-26 23:59:17 +0900

    1

--- 02.stack&queue/tower.py
+++ 02.stack&queue/tower.py
@@ -43,60 +43,64 @@ def solution(heights):
 
 '''
 
+
 def solution(heights):
     # heights = [5,3,1,2,3]
     answer = [0]
-    max = [(heights[0],0)] # max = [(높이,인덱스),....]
-    for i in range(1,len(heights)):
-        if heights[i]<heights[i-1]:
+    max = [(heights[0], 0)]  # max = [(높이,인덱스),....]
+    for i in range(1, len(heights)):
+        if heights[i] < heights[i - 1]:
             # 1.뒷놈이 앞놈보다 작을 경우
             answer.append(i)
-            max.append((heights[i-1],i-1))
-        elif heights[i]>heights[i-1]:
+            max.append((heights[i - 1], i - 1))
+        elif heights[i] > heights[i - 1]:
             # 2.뒷높이 앞놈보다 클 경우
             if max[-1][0] > heights[i]:
                 # 2-1.뒷놈이 지금까지 max보다 작을 경우
-                answer.append(max[-1][1]+1) # max의 index는 인덱스이므로 n번째 수로 치환하여 append
+                answer.append(max[-1][1] + 1)  # max의 index는 인덱스이므로 n번째 수로 치환하여 append
             elif max[-1][0] < heights[i]:
                 # 2-2.뒷놈이 지금까지 max보다 클 경우
                 answer.append(0)
-                max.append((heights[i],i))
+                max.append((heights[i], i))
             else:
                 # 2-3. 뒷놈과 max가 같을 경우
                 max.pop()
-                
-                while(max[-1][0]<=heights[i]):
+
+                while (max[-1][0] <= heights[i]):
                     max.pop()
                     if (max == []):
                         answer.append(0)
                         break;
                 if (max):
-                    answer.append(max[-1][1]+1)
-                max.append((heights[i],i))
-                    
+                    answer.append(max[-1][1] + 1)
+                max.append((heights[i], i))
+
         else:
             # 3.뒷놈과 앞놈이 같을경우
             if max[-1][0] > heights[i]:
-                answer.append(max[-1][1]+1)
-            else :
+                answer.append(max[-1][1] + 1)
+            else:
                 answer.append(0)
-                max.append((heights[i],i))
+                max.append((heights[i], i))
     return answer
 
 
 # 참고할만한 코드
 def solution(heights):
-    answer = [0]*len(heights)
-    stack = [] 
+    answer = [0] * len(heights)
+    stack = []
 
     for i in reversed(range(len(heights))):
         while stack and stack[-1][1] < heights[i]:
             idx, height = stack.pop()
-            answer[idx] = i+1
+            answer[idx] = i + 1
         stack.append((i, heights[i]))
 
     return answer
 
+
+if __name__ == '__main__':
+    print(solution([5, 3, 1, 2, 3]))
 '''
 기본적인 아이디어는 비슷하지만, reverse 또는 range의 스텝 -1로 거꾸로 가면서 max를 검사하는것이 핵심.
 그냥 쭉 따라가면서 다음 height보다 낮은 max를 pop해주고, 다시 갱신해주는 방식을 볼 것.
