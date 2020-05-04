22cab39c8a2c61ae64f0c68d3d7a8bc89f9d5fcd
SeongIkKim <kpic1638@gmail.com>
2020-05-05 02:12:28 +0900

    FAILED - 03.DFS&BFS target number

--- 03.DFS&BFS/target_number.py
+++ 03.DFS&BFS/target_number.py
@@ -2,20 +2,22 @@
 1.루트노드에 0을 넣어놓은 뒤(계산에 영향이 안가도록), 리스트의 원소 계산을 +로 할지 -로 할지에 따라 2분기되는 이진트리로 구상한다.
 2.하나의 원소는 하나의 level에 대응하므로, DFS로 탐색을 진행한다.
 3.만약 리프노드에 도달했다면, 루트노드로부터 거쳐온 경로의 계산값을 타겟넘버와 비교하고 같다면 count를 올린다.
-'''    
+''' 
 
+def dfs(visited,x,n,i,t,answer):
+    if len(visited) == len(n)-1:
+        if sum(visited) == t:
+            return answer+1
+    visited.append(x)
+    answer = dfs(visited,n[i],n,i+1,t,answer)
+    answer = dfs(visited,-n[i],n,i+1,t,answer)
+    
+    return answer
+
+        
 def solution(n, t):
     answer = 0
     visited = []
-    to_visit = [0] # 루트노드 0
+    answer = dfs(visited,0,n,0,t,answer)
     
-    end = len(n)-1
-    i=0
-    while to_visit:
-        now = to_visit.pop() # 현재 방문노드
-        if now not in visited:
-            visited.append(now)
-            to_visit.extend([n[i],-n[i]])
-            i+=1;
-        if i == end:
-            answer.append(sum[visited])
+    return answer
