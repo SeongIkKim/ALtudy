b650afab85087f3555d52891d8e5d79a8e2df57b
SeongIkKim <kpic1638@gmail.com>
2020-12-10 10:53:13 +0900

    [SOLVED] Linked List - 018 Odd Even Linked List Solution

--- LeetCode/Linked_List/018_Odd_Even_Linked_List.py
+++ LeetCode/Linked_List/018_Odd_Even_Linked_List.py
@@ -37,3 +37,31 @@ class Solution:
 생각은 바로 났는데 even.next = None으로 리스트 사이클을 벗어나기까지가 조금 걸렸다.
 '''
 
+# Solution
+
+class Solution:
+    def oddEvenList(self, head: ListNode) -> ListNode:
+        if head is None:
+            return None
+
+        odd = head
+        even_head = even = head.next
+
+        # even이 뒤쪽에 오므로 even 기준으로 while문을 돌리는 듯 하다
+        while even and even.next:
+            # 훨씬 직관적인 풀이
+            odd.next, even.next = odd.next.next, even.next.next
+            odd, even = odd.next, even.next
+
+        # 홀수 끝을 짝수 처음과 이어주기
+        odd.next = even_head
+        return head
+
+'''
+36ms(93.17%)
+16.4MB(20.95%)
+시간복잡도 공간복잡도 동일하지만, 더 직관적인 풀이
+1. root=ListNode(None)같은 쓸데없는 헤더를 만들지 않음
+2. while문 이하의 내용이 훨씬 직관적임
+3. while문의 조건을 odd가 아닌 even기준으로 잡았기 때문에 odd 끝부분과 even_head를 이어주는 과정이 간단
+'''
