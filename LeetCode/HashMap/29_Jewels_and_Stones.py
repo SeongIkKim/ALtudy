773d99db06009d8ec1b9d820609050ca9bbebeb9
SeongIkKim <kpic1638@gmail.com>
2020-12-26 20:28:55 +0900

    [SOLVED] HashMap - 29 Jewels and Stones Solution

--- LeetCode/HashMap/29_Jewels_and_Stones.py
+++ LeetCode/HashMap/29_Jewels_and_Stones.py
@@ -17,3 +17,31 @@ class Solution:
 32ms(55.38%)
 14.4MB(8.25%)
 '''
+
+# 1st solution - Counter
+
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        freq = collections.Counter(stones)
+        count = 0
+        for jewel in jewels:
+            count += freq[jewel]
+        return count
+
+'''
+32ms(55.38%)
+13.9MB(97.59%)
+Counter 사용해 stone을 딕셔너리에 집어넣는 과정을 생략함.
+'''
+
+# 2nd solution - pythonic
+
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        return sum(s in jewels for s in stones)
+'''
+32ms(55.38%)
+13.9MB(97.59%)
+s in jewels의 결과가 boolean으로 나오고, 리스트 컴프리헨션이므로,
+sum은 True의 갯수를 합쳐서 반환한다. 
+'''
