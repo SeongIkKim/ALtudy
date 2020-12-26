2a31deab5a0cd92a0194a17acb7f827a297bbdc3
SeongIkKim <kpic1638@gmail.com>
2020-12-26 22:02:52 +0900

    [FAILED] HashMap - Longest Substring Without Repeating Characters Solution

--- LeetCode/HashMap/30_Longest_Substring_Without_Repeating_Characters.py
+++ LeetCode/HashMap/30_Longest_Substring_Without_Repeating_Characters.py
@@ -27,3 +27,30 @@ class Solution:
 '''
 
 
+# solution
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        used = {}
+        max_length = start = 0  # pointer 1 : start
+        for index, char in enumerate(s):  # pointer 2 : index
+            # 이미 등장했던 철자라면 start 위치 갱신
+            # start <= used[char]를 체크해 start 위치를 반복 철자 다음 인덱스로 당겨옴
+            if char in used and start <= used[char]:
+                start = used[char] + 1
+                # 최대부분 문자열 길이 갱신
+            else:
+                max_length = max(max_length, index - start + 1)
+
+            # 현재 문자의 위치 삽입
+            used[char] = index
+
+        return max_length
+
+'''
+44ms(98.45%)
+14.4MB(39.81%)
+1. 투포인터 사용이 스무스해야함..(start와 index)
+2. enumerate를 사용한 간편한 index 및 철자구성
+3. max함수를 이용한 max_length 갱신
+'''
