81685ac1e9cdb1fb23e72623e52ec2333c5654f7
SeongIkKim <kpic1638@gmail.com>
2021-01-02 23:45:10 +0900

    [SOLVED] Graph - Numbers of Islands Solution

--- LeetCode/Graph/32_Numbers_of_Islands.py
+++ LeetCode/Graph/32_Numbers_of_Islands.py
@@ -101,3 +101,36 @@ discovered를 만들 필요 없이 그냥 0으로 만들면 된다(조언을 보
 리스트컴프리헨션문이 아주 괴랄한데, 갓python님의 -1,-2 등 음수 인덱스 지원때문이다(...)
 세상에, try except걸어놓은 ii, jj가 (0,-1)이 될 지는 몰랐다. 
 '''
+
+# solution
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        def dfs(i, j):
+            # 더 이상 grid 영역이 아니거나, 땅('1')이 아닌 경우 종료
+            if not (0 <= i < len(grid)) or not (0 <= j < len(grid[0])) or grid[i][j] != '1':
+                return
+
+            grid[i][j] = 0
+
+            # 동서남북 탐색
+            dfs(i + 1, j)
+            dfs(i - 1, j)
+            dfs(i, j + 1)
+            dfs(i, j - 1)
+
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    dfs(i, j)
+                    # 하나의 섬 탐색이 끝났음
+                    count += 1
+        return count
+
+'''
+136ms(76.62%)
+15.6MB(32.10%)
+초반 예외처리가 깔끔하여 이후 코드가 모두 우아해진다.
+재귀함수를 사용할 때 예외처리는 함수 머릿부분에 두려고 의식적으로 노력해보자.
+'''
