fcf35b39237f1b20fdc95a94bdd475ceee8c5a47
SeongIkKim <kpic1638@gmail.com>
2021-01-11 13:43:35 +0900

    [SOLVED] Graph - Cheapest_Flight_Within_K_Stops

--- LeetCode/Graph/41_Cheapest_Flight_Within_K_Stops.py
+++ LeetCode/Graph/41_Cheapest_Flight_Within_K_Stops.py
@@ -67,3 +67,41 @@ class Solution:
 Memory Limit Exceeded
 후.. 화난다.
 '''
+
+
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, K: int) -> int:
+        graph = collections.defaultdict(list)
+        for u, v, w in flights:
+            graph[u].append((v, w))
+        print(graph)
+
+        D = collections.defaultdict(lambda: sys.maxsize)
+
+        # 정점, 가격
+        Q = collections.deque([(src, 0)])
+
+        stop = 0
+        while stop <= K:
+            for _ in range(len(Q)):
+                fr, price = Q.popleft()
+                for v, w in graph[fr]:
+                    # 핵심은 가지치기. if문을 넣지 않으면 계산했던 D[v]의 다음 노드를 다시 큐에 넣어버린다..
+                    if min(D[v], price + w) == price + w:
+                        D[v] = min(D[v], price + w)
+                        Q.append((v, D[v]))
+            stop += 1
+
+        return D[dst] if D[dst] != sys.maxsize else -1
+
+'''
+84ms(83.48%)
+15.3MB(67.49%)
+팁 : sys.maxsize
+BFS
+1. 그래프 생성
+2. 큐 생성(첫 원소를 넣어둠)
+3. 전 단계에서 쌓아둔 큐 내역(for _ in range(len(Q))을 뽑으면서 다음 큐 내역을 뒤쪽에 쌓음.
+4. 이 때, 이미 찾은 원소를 가지치기 하지 않으면 메모리/시간 초과.
+'''
+
